63586f883de494de98d7210f9d4d41ee41518bdb
Ryan Gao <ygao@woolworths.com.au>
2023-12-01 14:00:11 +1100

    slution to channlenge 6

---
+++ src/leetcode/6.zigzag-conversion.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        ret = list()
+        row = 0
+        inc = 1
+        for c in s:
+            if len(ret) < row+1:
+                ret.append(list())
+            ret[row].append(c)
+            row = row + inc
+            if row == numRows-1 or row == 0:
+                inc = -1 * inc
+                    
+        ret_str = ""
+        for row in ret:
+            ret_str += "".join(row)
+        return ret_str
+    
+if __name__ == "__main__":
+    print(Solution().convert("PAYPALISHIRING", 3))
