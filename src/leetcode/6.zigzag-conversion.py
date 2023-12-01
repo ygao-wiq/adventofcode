56811d4274d7ac454f6f94be00cfbeaf8632bd8f
Ryan Gao <ygao@woolworths.com.au>
2023-12-01 14:19:10 +1100

    another opt again

--- src/leetcode/6.zigzag-conversion.py
+++ src/leetcode/6.zigzag-conversion.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def convert(self, s, numRows):
+    def convert1(self, s, numRows):
         """
         :type s: str
         :type numRows: int
@@ -22,6 +22,26 @@ class Solution(object):
         for row in ret:
             ret_str += "".join(row)
         return ret_str
-    
+    def convert(self, s, numRows):
+        """
+        :type s: str
+        :type numRows: int
+        :rtype: str
+        """
+        if numRows == 1 or len(s) == 0:
+            return s
+        ret = list()
+        doubleLen = 2*numRows - 2
+        i = 0
+        while i<numRows:
+            j = 0
+            while i+j < len(s):
+                ret.append(s[i+j])
+                if i != 0 and i != numRows-1 and j+doubleLen-i < len(s):
+                    ret.append(s[j+doubleLen-i])
+                j = j+ doubleLen
+            i += 1
+        return "".join(ret)
+
 if __name__ == "__main__":
     print(Solution().convert("PAYPALISHIRING", 3))
