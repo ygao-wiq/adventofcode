e661f3628298b27531bf889b64cb36fc74e70e43
ygao-wiq <ygao@woolworths.com.au>
2024-02-01 21:42:49 +1100

    solution done with leetcode challenge 33

---
+++ src/leetcode/33.search-in-rotated-sorted-array.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode id=33 lang=python3
+#
+# [33] Search in Rotated Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums)-1
+        while left <= right:
+            mid = (left+right) // 2
+            if target == nums[mid]:
+                return mid
+            elif nums[mid] >= nums[left]:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+        return -1
+# @lc code=end
+
