9678364f1d21dedbf502c92d00ca32e910dff5ae
ygao-wiq <ygao@woolworths.com.au>
2025-04-27 09:27:50 +1000

    solution to leetcode challenge 86 done

---
+++ src/leetcode/86.partition-list.py
@@ -0,0 +1,37 @@
+#
+# @lc app=leetcode id=86 lang=python3
+#
+# [86] Partition List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+
+        if not head:
+            return None
+
+        less_head = less = ListNode(0)
+        greater_head = greater = ListNode(0)
+
+        while head:
+            if head.val < x:
+                less.next = head
+                less = less.next
+            else:
+                greater.next = head
+                greater = greater.next
+            head = head.next
+
+        greater.next = None
+        less.next = greater_head.next
+
+        return less_head.next
+        
+# @lc code=end
+
