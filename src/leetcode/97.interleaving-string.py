829897b196c1c88c0d7bcfa6e1d5ce4e7630ba37
ygao-wiq <ygao@woolworths.com.au>
2025-10-05 10:44:56 +1100

    solution done with leetcode challenge 97

---
+++ src/leetcode/97.interleaving-string.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=97 lang=python3
+#
+# [97] Interleaving String
+#
+
+# @lc code=start
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+        dp = [False for _ in range(len(s2) + 2)]
+
+        for i in range(len(s1) + 1):
+            for j in range(len(s2) + 1):
+                if i == 0 and j == 0:
+                    dp[j] = True
+                elif i == 0:
+                    dp[j] = dp[j-1] and s2[j-1] == s3[i+j-1]
+                elif j == 0:
+                    dp[j] = dp[j] and s1[i-1] == s3[i+j-1]
+                else:
+                    dp[j] = (dp[j] and s1[i-1] == s3[i+j-1]) or (dp[j-1] and s2[j-1] == s3[i+j-1])
+        return dp[len(s2)]
+
+
+        
+# @lc code=end
+
