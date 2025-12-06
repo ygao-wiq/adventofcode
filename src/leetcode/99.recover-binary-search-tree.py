f934c3387aef11b4ab89095da7b2bb14f7decd83
ygao-wiq <ygao@woolworths.com.au>
2025-12-07 10:49:55 +1100

    solution done for leetcode challenge 99

---
+++ src/leetcode/99.recover-binary-search-tree.py
@@ -0,0 +1,40 @@
+#
+# @lc app=leetcode id=99 lang=python3
+#
+# [99] Recover Binary Search Tree
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def recoverTree(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        stack = []
+        x = y = pred = None
+
+        while stack or root:
+            while root:
+                stack.append(root)
+                root = root.left
+            root = stack.pop()
+            if pred and root.val < pred.val:
+                y = root
+                if x is None:
+                    x = pred
+                else:
+                    break
+            pred = root
+            root = root.right
+
+        x.val, y.val = y.val, x.val
+
+        
+# @lc code=end
+
